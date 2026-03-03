deb1794126aa4d6571fda96a7fa0fbd1c31bb9e6
raulzinkkj <karvatraul@gmail.com>
2026-03-03 14:24:43 -0300

    oloco

--- index.php
+++ index.php
@@ -9,33 +9,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
         body {
             font-family: Arial;
             text-align: center;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
         }
 
         h1 {
-            margin-top: 75px;
-        }
+            padding: 15px; 
+            font-size: 3rem;
+            font-weight: 400;
+        }   
 
         h2 {
-            border-bottom: solid 1px black;
-            padding-bottom: 10px;
-        }
+            padding: 15px;              
+            background: #F5F5F5;    
+            font-weight: 400;
+            border-bottom: solid 1px #d1d0d0;   
+        }       
 
         .container {
-            display: flex;
-            justify-content: space-around;
-            gap: 30px;
-            margin-top: 150px;
+            display: flex;  
+            justify-content: space-evenly;
+            padding: 20px;
         }
 
         .card {
-            border-radius: 8px;
-            width: 300px;
-            padding: 20px;
-            border: solid 1px black;
-        }
+            border-radius: 5px;
+            width: 500px;
+            border: solid 1px #d1d0d0;
+            text-align: left;
+        }   
 
         button {
             background-color: #2c6faa;
@@ -43,13 +54,26 @@
             padding: 10px 30px;
             border-radius: 5px;
             border: none;
+            margin: 10px 0px 15px 15px;
+            font-size: 1.2rem;
         }
+        p {
+            padding: 15px;  
+            font-size: 1.3rem;
+        }
+        .titulo, .texto {
+            margin: 0;  
+            padding: 0;
+        } 
     </style>
 </head>
 
 <body>
-    <h1>Sistema de Gerenciamento - SENAI</h1>
-    <p>O sistema utilizado por todas as unidades do senai em todo o Brasil, utilizada por mais de 5.000.000 de alunos.</p>
+    <div class="titulo">
+        <h1>Sistema de Gerenciamento - SENAI</h1> 
+        <p style="font-size: 2rem; padding: 0;">O sistema utilizado por todas as unidades do senai em todo o Brasil,</p>
+        <p style="font-size: 2rem; padding: 0;"> utilizada por mais de 5.000.000 de alunos.</p>
+    </div>
 
     <div class="container">
         <div class="card">
